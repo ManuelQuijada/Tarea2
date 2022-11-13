6d78e8ec801b243e2c3e2aabc25012c0871c586c
ManuelQuijada <manuel.quijada1901@alumnos.ubiobio.cl>
2022-11-13 15:28:10 -0300

    Tarea

---
+++ InfoIP.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+#include<thread>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+
+using namespace std;
+
+int cont = 0;
+
+string info (string data){
+    int i = 0;
+    char buffer[100];
+    string result = "";
+    FILE* pipe = popen(data.c_str(), "r");
+    if(!pipe){
+        cout << "No se pudo ejecutar" << endl;
+    }
+    while(!feof(pipe)){
+        if(fgets(buffer, 100, pipe) != NULL)
+            result +=buffer;
+    }
+    pclose(pipe);
+    return result;
+}
+
+int main(int argc, char *argv[]){
+    int i = 0;
+    int c_lineas = 0;
+    ifstream cantidad(argv[1]);
+    string lineas;
+    while(getline(cantidad, lineas)){
+        c_lineas++;
+    }
+    cantidad.close();
+    cout << "IP           Trans.      Rec.     Perd.      Estado"<< endl;
+    cout << "---------------------------------------------------"<< endl;
+    string lista[c_lineas];
+    ifstream cantip(argv[1]);
+    string tip;
+    while(getline(cantip,tip)){
+        lista[i]=tip;
+        i++;
+    }
+    cantip.close();
+    string data;
+    thread threads[c_lineas];
+    for (i=0; i < c_lineas; i++) {
+        data = "ping " + lista[i] + " -c " + argv[2];
+        threads[i] = thread(info, data);
+    }
+
+    for (i=0; i< c_lineas; i++) {
+        threads[i].join();
+    }
+    string packetr = "";
+    string ls = info("ls");
+    int busqueda = 0;
+    for (int i= 0 ; i< ls.length(); i++){
+        if (ls[i] == ','){
+            for(int j=i+2; ls[j]!= ' '; j++){
+                packetr += ls[j];
+            }
+            break;
+        }
+    }
+    string valor;
+    int packt = atoi(argv[2]);
+    int pack = stoi(packetr);
+    int packresu = packt - pack;
+    if (pack>0){
+        valor = "UP";
+    }else{
+        valor = "DOWN";
+    }
+    for (int i= 0; i<c_lineas; i++){
+        cout << lista[i] << "     " << packt << "   " << pack << "   " << packresu << "   " << valor << endl;
+    }
+    return 0;
+}
---
+++ Makefile.mak
@@ -0,0 +1,5 @@
+all:
+    g++ -o InfoIP InfoIP.cpp -lpthread
+    ./InfoIP listado.txt 4
+clean:
+    rm InfoIP
